4ca3cdfe66d90f99fe2ebd412bcbdd6e2ee0094b
Dorian Andrés <dcortesg441@ulacit.ed.cr>
2024-06-11 00:30:31 -0600

    Adding auto-clicks

--- src/app/src/main/java/com/byro/kittensclicker/MainActivity.kt
+++ src/app/src/main/java/com/byro/kittensclicker/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -15,16 +18,20 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.byro.kittensclicker.ui.theme.KittensClickerTheme
+import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.seconds
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +39,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KittensClickerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize().safeDrawingPadding()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .safeDrawingPadding()
+                ) { innerPadding ->
                     Root(modifier = Modifier.padding(innerPadding))
                 }
             }
@@ -42,15 +53,45 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Root(modifier: Modifier = Modifier) {
-    var count by remember { mutableIntStateOf(0) }
+    val interactionSource = remember { MutableInteractionSource() }
+    var clicksCount by remember { mutableIntStateOf(0) }
+    var autoClicks by remember { mutableIntStateOf(0) }
+    var autoClickCost by remember { mutableIntStateOf(10) }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(2.seconds)
+            for (i in 0 until autoClicks) {
+                clicksCount++
+            }
+        }
+    }
 
     Column(
-        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { count++ }) {
-            Text("Click me! ($count)")
+        Text("Score: $clicksCount", fontSize = 62.sp)
+        Image(
+            painter = painterResource(id = R.drawable.default_cat),
+            contentDescription = stringResource(id = R.string.default_cat_description),
+            modifier = Modifier.clickable(
+                onClick = { clicksCount++ },
+                interactionSource = interactionSource,
+                indication = null
+            )
+        )
+        Button(onClick = {
+            if (clicksCount >= autoClickCost) {
+                clicksCount -= autoClickCost;
+                autoClickCost *= 10;
+                autoClicks++;
+            }
+        }) {
+            Text(text = "Buy 1 auto-click (₡ ${autoClickCost})", fontSize = 30.sp)
         }
     }
 }
