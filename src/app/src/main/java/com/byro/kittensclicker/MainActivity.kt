f05e0c3543efe682c738a9bb29057710ba6ad92e
Dorian Andrés <dcortesg441@ulacit.ed.cr>
2024-06-10 23:17:03 -0600

    Adding click counter

--- src/app/src/main/java/com/byro/kittensclicker/MainActivity.kt
+++ src/app/src/main/java/com/byro/kittensclicker/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.byro.kittensclicker.ui.theme.KittensClickerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +32,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KittensClickerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize().safeDrawingPadding()) { innerPadding ->
                     Root(modifier = Modifier.padding(innerPadding))
                 }
             }
@@ -29,10 +42,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Root(modifier: Modifier = Modifier) {
-    Text(
-        text = "Kittens Clicker :)",
-        modifier = modifier
-    )
+    var count by remember { mutableIntStateOf(0) }
+
+    Column(
+        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { count++ }) {
+            Text("Click me! ($count)")
+        }
+    }
 }
 
 @Preview(showBackground = true)
